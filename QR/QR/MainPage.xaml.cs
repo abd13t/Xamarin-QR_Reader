66f81565e0199407b304b21c7406f3ecb203195d
abd13t <a.hadjadj.aoul@gmail.com>
2018-09-13 15:59:57 +0100

    Add files via upload

---
+++ QR/QR/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using QR.Services;
+using System;   
+using Xamarin.Forms;  
+  
+  
+namespace QR
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnScan_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var scanner = DependencyService.Get<IQrScanningService>();
+                var result = await scanner.ScanAsync();
+                if (result != null)
+                {
+                    txtBarcode.Text = result;
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("error", ex.ToString(), "ok");
+                //throw;
+            }
+        }
+    }
+}
---
+++ QR/QR/Services/IQrScanningService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QR.Services
+{
+    public interface IQrScanningService
+    {
+        Task<string> ScanAsync();
+    }
+
+}
